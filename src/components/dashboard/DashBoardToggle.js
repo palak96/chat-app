16dc0743c6ba089e7da6210a5061ce89687e07e5
palak96 <palak.telang@gmail.com>
2021-05-19 22:49:45 +0530

    started creating dashboard;added username and SignOut button and functionality

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -1,12 +1,19 @@
 /* eslint-disable arrow-body-style */
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
 import { useMediaQuery, useModalState } from '../../context/custom-hooks';
+import { auth } from '../../misc/firebase';
 
 const DashBoardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width : 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed Out', 4000);
+    close();
+  }, [close]);
 
   return (
     <>
@@ -14,7 +21,7 @@ const DashBoardToggle = () => {
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,8 +1,27 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-  return <div>x</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>DashBoard</Drawer.Title>
+      </Drawer.Header>
+      <Drawer.Body>
+        <h3>Hey, {profile.name}</h3>
+      </Drawer.Body>
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          {' '}
+          Sign Out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;

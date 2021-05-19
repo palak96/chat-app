c7324754eeb970a812de9361915b00cb204bc0ba
palak96 <palak.telang@gmail.com>
2021-05-19 20:37:08 +0530

    manage user with profile context;put real time subs

---
+++ src/context/profile.context.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { auth, database } from '../misc/firebase';
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+    const authUnsub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+
+          const data = {
+            uid: authObj.uid,
+            email: authObj.email,
+            name,
+            createdAt,
+          };
+
+          setProfile(data);
+          setIsLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        setProfile(null);
+        setIsLoading(false);
+      }
+    });
+    return () => {
+      authUnsub();
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{ isLoading, profile }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => useContext(ProfileContext);

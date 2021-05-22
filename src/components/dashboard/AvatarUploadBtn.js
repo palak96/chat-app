88e0e5c97a469ef317400eea2c46b26b8ecbd4d3
palak96 <palak.telang@gmail.com>
2021-05-22 08:09:45 +0530

    uploaded user avatar

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,19 +1,36 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import AvatarEditor from 'react-avatar-editor';
 import { Alert, Button, Modal } from 'rsuite';
 import { useModalState } from '../../context/custom-hooks';
+import { storage, database } from '../../misc/firebase';
+import { useProfile } from '../../context/profile.context';
 
 const fileInputTypes = '.png,.jpeg,.jpg';
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
 
 const AvatarUploadBtn = () => {
   const { isOpen, open, close } = useModalState();
+  const avatarEditorRef = useRef();
+  const { profile } = useProfile();
+  const [isLoading, setIsLoading] = useState(false);
 
   const [img, setImg] = useState(null);
   const isVaildFiles = file => acceptedFileTypes.includes(file.type);
+
+  const getBlob = canvas => {
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error('File process error'));
+        }
+      });
+    });
+  };
 
   const onFileInputChange = ev => {
     const currFiles = ev.target.files;
@@ -27,6 +44,33 @@ const AvatarUploadBtn = () => {
       }
     }
   };
+
+  const onUploadClick = async () => {
+    setIsLoading(true);
+    const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+    try {
+      const blob = await getBlob(canvas);
+      const avatarFileRef = storage
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+
+      const uploadAvatarResult = await avatarFileRef.put(blob, {
+        cacheControl: `public, max-age ${3600 * 24 * 3} `,
+      });
+      const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
+      const userAvatarRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+
+      userAvatarRef.set(downloadUrl);
+      setIsLoading(false);
+      Alert.info('Avatar has been uploaded', 4000);
+    } catch (err) {
+      setIsLoading(false);
+      Alert.error(err.message, 4000);
+    }
+  };
+
   return (
     <div className="mt-3 text-center">
       <div>
@@ -45,12 +89,13 @@ const AvatarUploadBtn = () => {
         </label>
         <Modal show={isOpen} onHide={close}>
           <Modal.Header>
-            <Modal.Title>Adjust and upload new avatar</Modal.Title>
+            <Modal.Title> Adjust and upload new avatar</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <div className="d-flex justify-content-center align-items-center h-100">
               {img && (
                 <AvatarEditor
+                  ref={avatarEditorRef}
                   image={img}
                   width={200}
                   height={200}
@@ -62,7 +107,12 @@ const AvatarUploadBtn = () => {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button block appearance="ghost">
+            <Button
+              block
+              appearance="ghost"
+              onClick={onUploadClick}
+              disabled={isLoading}
+            >
               Upload new avatar
             </Button>
           </Modal.Footer>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -3,6 +3,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyBW9KmcMAwI8Jkqj-qN80TYntlT7vkMO3A',
@@ -17,3 +18,4 @@ const config = {
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();

6994fc3e971c869c65861fc69634f61b15db8e04
palak96 <palak.telang@gmail.com>
2021-05-26 11:20:56 +0530

    started to build chat top ;created room info

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,10 +1,39 @@
 /* eslint-disable arrow-body-style */
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
+import { useMediaQuery } from '../../../context/custom-hooks';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width:992px)');
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            size="2x"
+            icon="arrow-circle-left"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 text-blue mr-2  link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(Top);

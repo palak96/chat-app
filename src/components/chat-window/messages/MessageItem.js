8e9719de2bc362f8128db19afeb0cebefc7c83ea
palak96 <palak.telang@gmail.com>
2021-05-27 23:00:06 +0530

    displayed messages inside chat page

---
+++ src/components/chat-window/messages/MessageItem.js
@@ -0,0 +1,31 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../../ProfileAvatar';
+
+const MessageItem = ({ message }) => {
+  const { author, createdAt, text } = message;
+
+  return (
+    <li className="padded mb-1">
+      <div className="d-flex align-items-center mb-1 font-bolder">
+        <ProfileAvatar
+          src={author.avatar}
+          name={author.name}
+          className="ml-1"
+          size="xs"
+        />
+        <span className="ml-2">{author.name}</span>
+        <TimeAgo
+          className="font-normal text-black-45 ml-2"
+          datetime={createdAt}
+        />
+      </div>
+      <div>
+        <span className="word-break-all">{text}</span>
+      </div>
+    </li>
+  );
+};
+
+export default MessageItem;
